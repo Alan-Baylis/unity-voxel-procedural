74533e9ad3d640e8e26c942b876d2f26dca0196a
pebcak <d3k41n@gmail.com>
2017-05-28 10:20:36 +0100

    Added chunk creation

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -4,9 +4,7 @@
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
 public class Chunk : MonoBehaviour {
-    Block[,,] blocks;
-
-    public static int chunkSize = 16;
+    public static int ChunkSize = 16;
 
     public bool update = true;
 
@@ -14,38 +12,51 @@ public class Chunk : MonoBehaviour {
 
     MeshCollider col;
 
+    public World world;
+
+    public WorldPos pos;
+
+    private Block[,,] blocks = new Block[ChunkSize, ChunkSize, ChunkSize];
+
     // Use this for initialization
     void Start() {
         filter = gameObject.GetComponent<MeshFilter>();
         col = gameObject.GetComponent<MeshCollider>();
-        //past here is just to set up an example chunk
-        blocks = new Block[chunkSize, chunkSize, chunkSize];
-        for (int x = 0; x < chunkSize; x++) {
-            for (int y = 0; y < chunkSize; y++) {
-                for (int z = 0; z < chunkSize; z++) {
-                    blocks[x, y, z] = new BlockAir();
-                }
-            }
-        }
-        blocks[3, 5, 2] = new Block();
-        blocks[4, 5, 2] = new BlockGrass();
-        UpdateChunk();
     }
 
     //Update is called once per frame
     void Update() {
+        if (update) {
+            update = false;
+            UpdateChunk();
+        }
     }
 
     public Block GetBlock(int x, int y, int z) {
-        return blocks[x, y, z];
+        if (InRange(x) && InRange(y) && InRange(z))
+            return blocks[x, y, z];
+        return world.GetBlock(pos.x + x, pos.y + y, pos.z + z);
+    }
+
+    public void SetBlock(int x, int y, int z, Block block) {
+        if (InRange(x) && InRange(y) && InRange(z)) {
+            blocks[x, y, z] = block;
+        } else {
+            world.SetBlock(pos.x + x, pos.y + y, pos.z + z, block);
+        }
+    }
+
+    //new function
+    public static bool InRange(int index) {
+        return index >= 0 && index < ChunkSize;
     }
 
     // Updates the chunk based on its contents
     void UpdateChunk() {
         MeshData meshData = new MeshData();
-        for (int x = 0; x < chunkSize; x++) {
-            for (int y = 0; y < chunkSize; y++) {
-                for (int z = 0; z < chunkSize; z++) {
+        for (int x = 0; x < ChunkSize; x++) {
+            for (int y = 0; y < ChunkSize; y++) {
+                for (int z = 0; z < ChunkSize; z++) {
                     meshData = blocks[x, y, z].Blockdata(this, x, y, z, meshData);
                 }
             }
